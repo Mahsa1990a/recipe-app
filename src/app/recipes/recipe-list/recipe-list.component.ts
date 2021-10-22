690d8df32abbd60920401c816648f83019959030
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-10-22 18:22:29 -0400

    adding the type of recipes to be Recipe Model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../../recipes/recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  // the type is Recipe Model
+  recipes: Recipe[] = [];
 
   constructor() { }
 
