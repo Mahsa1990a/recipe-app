d70522c232c91ff718c1017baee3c22dad8d2382
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-10-25 18:09:29 -0400

    87. Passing Recipe Data with Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,3 +22,9 @@ export class AppComponent {
 // 3.app template =>listen to featureSelected event from header and execute onNavigate($event)
 // 4. app ts => create onNavigate methode to update loadedFeature property
 // 5. in app html based on ngIf load recipes or shopping list
+
+// 87. Passing Recipe Data with Property Binding
+// 1.cut a tag: recipe item from recipe-list component and paste into recipe-item template
+// 2.cut *ngFor loop from recipe-item into recipe-list template
+// 3.add recipe property in recipe-item ts and make it bindable from outside(means recipe-item) with @Input
+// 4.now in recipe-list template bind into recipe property and equal it into item of our for loop
