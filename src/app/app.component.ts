8614fb4f525314668f1d58cca3ae13f16d363eb3
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-10-25 17:17:31 -0400

    86. Adding Navigation with Event Binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+
 }
+
+
+// PART 86. Adding Navigation with Event Binding and ngIf:
+// 1.add click event (click)="onSelect('recipe')" and (click)="onSelect('shopping-list')" in header
+// 2.add onSelect function ih header ts and create my own event with emit it(featureSelected) inside onSelect method
+// 3.app template =>listen to featureSelected event from header and execute onNavigate($event)
+// 4. app ts => create onNavigate methode to update loadedFeature property
+// 5. in app html based on ngIf load recipes or shopping list
