4508099b9d3c08f34d7a88d4bb415d4a5ddedeb7
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-10-27 12:38:16 -0400

    104.Closing the dropdown with clicking anywhere in the browser

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,5 +1,4 @@
-import { style } from "@angular/animations";
-import { Directive, HostBinding, HostListener, Input, OnInit } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core";
 
 @Directive({
   selector: '[appDropdown]'
@@ -11,8 +10,13 @@ export class DropdownDirective {
   // bind to class propety of that component(class is an arr of all classes) //open event
   @HostBinding('class.open') isOpenBehav = false;
 
-  @HostListener('click') toggleOpen() { //after click if T => F or if F => T
-    this.isOpenBehav = !this.isOpenBehav;
+  // @HostListener('click') toggleOpen() { //after click if T => F or if F => T
+  //   this.isOpenBehav = !this.isOpenBehav;
+  // }    UPDATE to click anywhere for closing dropdown:
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpenBehav = this.elRef.nativeElement.contains(event.target) ? !this.isOpenBehav : false;
   }
+
+  constructor(private elRef: ElementRef) {}
 };
 // after this we just need to add appDropdown into main attributes for toggle in header and detail
