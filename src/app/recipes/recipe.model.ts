3063029fa718b861348752c2dbbc34d78cd599f0
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-10-31 12:48:30 -0400

    122. Adding Ingredients to Recipes, with adding more argument to recipe model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,13 +1,18 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 // it's a model (model is a ts file)
 export class Recipe {
   // we can define how the recipe should look like in this class:
   public name: string;
   public description: string;
   public imagePath: string;
+  // now we add ingredients:
+  public ingredients: Ingredient[];
 
-  constructor(name: string, desc: string, imagePath: string) {
+  constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
     this.name = name;
     this.description = desc;
     this.imagePath = imagePath;
+    this.ingredients = ingredients;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 // managing recipes and datas
 
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -10,8 +11,25 @@ export class RecipeService {
   // the type is Recipe Model
   private recipes: Recipe[] = [
     //create a new obj based on our Recipe class:
-    new Recipe("A Test Recipe", "This is TEST", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
-    new Recipe("Anotherrrrrr Test Recipe", "This is TEST", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg")
+    new Recipe(
+      "Tasty steak",
+      "Super tasty steak .... This is awesome!",
+      "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg",
+      [
+        new Ingredient("Meat", 3),
+        new Ingredient("French fries", 20)
+      ]
+      ),
+    new Recipe(
+      "Big Fat Burger",
+      "What else you need to say?",
+      "https://upload.wikimedia.org/wikipedia/commons/0/0b/RedDot_Burger.jpg",
+      [
+        new Ingredient("Meat", 5),
+        new Ingredient("French fries", 20),
+        new Ingredient("Pepsi", 3) // then in recipe details we can output them
+      ]
+    )
   ];
 
   getRecipes() {
