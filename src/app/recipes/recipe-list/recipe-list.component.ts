70ebe76d606b9599c019e761ed8f31eb06eb2631
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-10-31 10:41:22 -0400

    118. Managing Recipes in a Recipe Service => getting recipes arr from recipeService

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from "../../recipes/recipe.model";
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -13,13 +14,16 @@ export class RecipeListComponent implements OnInit {
   // the type is Recipe Model
   recipes: Recipe[] = [
     //create a new obj based on our Recipe class:
-    new Recipe("A Test Recipe", "This is TEST", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
-    new Recipe("Anotherrrrrr Test Recipe", "This is TEST", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg")
+    // new Recipe("A Test Recipe", "This is TEST", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
+    // new Recipe("Anotherrrrrr Test Recipe", "This is TEST", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg")
+    // we want to get recipes from service:
   ];
 
-  constructor() { }
+  // we need to inject our service:
+  constructor(private recipeServiceProp: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeServiceProp.getRecipes();//get copy of recipe arr from service
   }
 
   onRecipeSelected(recipe: Recipe) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,17 @@
 // managing recipes and datas
-export class RecipeService {
 
+import { Recipe } from "./recipe.model";
+
+export class RecipeService {
+  // the type is Recipe Model
+  private recipes: Recipe[] = [
+    //create a new obj based on our Recipe class:
+    new Recipe("A Test Recipe", "This is TEST", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"),
+    new Recipe("Anotherrrrrr Test Recipe", "This is TEST", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg")
+  ];
+
+  getRecipes() {
+    return this.recipes.slice(); //this will return new arr which is copy of this arr in this service
+    // So with this ^ we get a copy of recipes arr for using outside
+  }
 }
