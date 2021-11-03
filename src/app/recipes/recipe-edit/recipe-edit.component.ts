b1a9b729790964cf6f8a3bc32fa2c064dc3248ef
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-11-03 18:13:01 -0400

    166. Retrieving Route Parameters

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeEditComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  // editing recipe or create new one:
+  editMode = false;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          //whenever premeters change:
+          this.editMode = params['id'] != null;//(params['id'] != null) means does it have the id
+          console.log("EditMode => ", this.editMode);
+          // if it doesn't have id(.../recipes/new) => editMode is false, if it has an id(.../recipes/id/edit) => editMode is true
+        }
+      )
   }
 
 }
