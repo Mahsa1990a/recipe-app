f7c5bcea72afa68a8db2214a6d6f0c3af78d586e
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-11-03 12:40:44 -0400

    157. Adding Navigation to the App(in header)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +10,9 @@ export class HeaderComponent {
   collapsed = true;
 
   // want to emit my own event and make it listenable from app component:
-  @Output() featureSelected = new EventEmitter<string>();
+  // @Output() featureSelected = new EventEmitter<string>(); Dont need it after adding routes
 
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
+  // onSelect(feature: string) {
+  //   this.featureSelected.emit(feature);
+  // }   Dont need it after adding routes
 };
